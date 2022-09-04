144ce514a8a153459ed177d967522afedd879cb4
Diana <artemida91510191@gmail.com>
2022-09-04 20:13:56 +0200

    wip3

--- src/ergast/fetchRequest.js
+++ src/ergast/fetchRequest.js
@@ -1,10 +1,10 @@
 export const getWorldChampionsStandings = async () => {
   let response = await fetch('http://ergast.com/api/f1/driverstandings/1.json?limit=99&offset=55')
-  return (await response.json()).MRData?.StandingsTable?.StandingsLists
+  return (await response.json())?.MRData?.StandingsTable?.StandingsLists
 }
 
 export const getSeasonStandings = async (season) => {
   let response = await fetch(`https://ergast.com/api/f1/${season}/results/1.json?limit=50`)
-  return (await response.json()).MRData?.RaceTable?.Races;
+  return (await response.json())?.MRData?.RaceTable?.Races;
 }
 
